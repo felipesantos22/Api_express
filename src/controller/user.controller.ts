d1a2c0dd46d37a94e11828db8d66a89a7cdf5d46
felipesantos22 <felipevs2018@gmail.com>
2023-05-31 16:14:11 -0300

    Implementando rota update

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -7,8 +7,15 @@ async function createUserController(req: Request, res: Response) {
     return res.status(200).json(newUser);
 }
 
-async function readUserontroller(_req: Request, res: Response) {
+async function readUserController(_req: Request, res: Response) {
     const readUser = await userController.readUserService();
+    return res.status(200).json(readUser);
+}
+
+async function updateUserController(req: Request, res: Response) {
+    const { id } = req.params;
+    const { email } = req.body;
+    const readUser = await userController.updateUserService({id, email});
     return res.status(200).json(readUser);
 }
 
@@ -18,5 +25,5 @@ async function deleteUserController(req: Request, res: Response) {
     return res.status(200).json({message:"User deleted!"});
 }
 
-export default { createUserController, readUserontroller, deleteUserController };
+export default { createUserController, readUserController, deleteUserController };
 
